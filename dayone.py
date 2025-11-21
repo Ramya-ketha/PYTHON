abb7062d546c7563cf2466096f74f77ec7a437b8
Ramya-ketha <ramyaketha13@gmail.com>
2025-11-21 17:53:47 +0530

    Update dayone.python

--- dayone.py
+++ dayone.py
@@ -1,1 +1,17 @@
 print("Today is day one")
+
+n=[1,2,5,8,9,11,5,7,16,7,95]
+print(n[1: :2])
+
+n=[12,7,8,9,15]
+print(len(n))
+print(sum(n))
+print(max(n))
+print(min(n))
+
+n=[[1,2,3],[4,5,6]]
+print(sum(sum(n,[])))
+
+n=["India","china","UK","canada","Russia"]
+n.sort(reverse = True)
+print(n)
